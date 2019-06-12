2bb741eaa4b79df0fa235be2d21ea287f950e6ac
Ryan Vieira Souza <ryanbanido2.0@gmail.com>
2019-06-11 22:30:39 -0300

    Validação dos nomes dos jogadores

--- js/principal.js
+++ js/principal.js
@@ -16,3 +16,23 @@ matriz_jogo ['b'] [3] = 0;
 matriz_jogo ['c'] [1] = 0;
 matriz_jogo ['c'] [2] = 0;
 matriz_jogo ['c'] [3] = 0;
+
+$(document).ready(function(){
+
+  $("#botao-start").click(function(){
+
+    //Validação dos nomes dos Jogadores
+
+    if ($("#jogador-um").val() == "") {
+        alert('O nome do primeiro jogador está vazio');
+        return false;
+    }
+    if ($("#jogador-dois").val() == "") {
+        alert('O nome do segundo jogador está vazio');
+        return false;
+    }
+
+    
+
+  })
+});
