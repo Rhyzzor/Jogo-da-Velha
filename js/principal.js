5286ee7bdada9996e316c98100eed58c9b4445fb
Ryan Vieira Souza <ryanbanido2.0@gmail.com>
2019-06-11 22:22:58 -0300

    Criação de matrizes (Arrays)

---
+++ js/principal.js
@@ -0,0 +1,18 @@
+var rodada = 1;
+var matriz_jogo = Array(3);
+
+matriz_jogo ['a'] = Array (3);
+matriz_jogo ['b'] = Array (3);
+matriz_jogo ['c'] = Array (3);
+
+matriz_jogo ['a'] [1] = 0;
+matriz_jogo ['a'] [2] = 0;
+matriz_jogo ['a'] [3] = 0;
+
+matriz_jogo ['b'] [1] = 0;
+matriz_jogo ['b'] [2] = 0;
+matriz_jogo ['b'] [3] = 0;
+
+matriz_jogo ['c'] [1] = 0;
+matriz_jogo ['c'] [2] = 0;
+matriz_jogo ['c'] [3] = 0;
