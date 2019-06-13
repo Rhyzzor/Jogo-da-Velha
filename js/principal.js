3dba438d95f0d7fc5b8e8500f0d5dea63e7c53d1
Ryan Vieira Souza <ryanbanido2.0@gmail.com>
2019-06-12 21:47:06 -0300

    Adicionado verificador de vencedores

--- js/principal.js
+++ js/principal.js
@@ -1,6 +1,7 @@
 var rodada = 1;
 var matriz_jogo = Array(3);
 
+
 matriz_jogo ['a'] = Array (3);
 matriz_jogo ['b'] = Array (3);
 matriz_jogo ['c'] = Array (3);
@@ -46,9 +47,9 @@ $(document).ready(function(){
   });
   //Contador de Click na área
   $(".espaco").click(function(){
-    var espacoSelecionado = this.id;
-    $("#" + espacoSelecionado).off();
-    movimento(espacoSelecionado);
+    var campoClicado = this.id;
+    $("#"+campoClicado).off();
+    movimento(campoClicado);
   });
 
   //Verificador de pontos e acionador de "icones (X ou O)"
@@ -57,14 +58,14 @@ $(document).ready(function(){
     var icone = "";
     var ponto = 0;
 
-    if (rodada % 2 == 1){
+    if (rodada % 2 == 1) {
 
-      icone = 'url("imagens/cruz.png")'
+      icone = 'url("imagens/cruz.png")';
       ponto = -1;
     } else {
 
-      icone = 'url("imagens/bolinha.png")'
-      ponto = 1
+      icone = 'url("imagens/bolinha.png")';
+      ponto = 1;
     }
 
     rodada++;
@@ -72,8 +73,8 @@ $(document).ready(function(){
     $("#" + id).css('background-image', icone);
 
     var linhaColuna = id.split('-');
-
-    matriz_jogo[linhaColuna[0]][linhaColuna[1]] = pontos;
+    matriz_jogo[linhaColuna[0]][linhaColuna[1]] = ponto;
+    checandoCombinacao();
   }
 
   function checandoCombinacao() {
@@ -81,24 +82,24 @@ $(document).ready(function(){
     //Verificador na horizontal
     var pontos = 0;
     for (var cont = 1; cont <= 3; cont++){
-      pontos = pontos + matriz_jogo['a'][i];
+      pontos = pontos + matriz_jogo['a'][cont];
     }
     vencedor(pontos);
 
     pontos = 0;
     for (var cont = 1; cont <= 3; cont++){
-      pontos = pontos + matriz_jogo['b'][i];
+      pontos = pontos + matriz_jogo['b'][cont];
     }
     vencedor(pontos);
 
     pontos = 0;
     for (var cont = 1; cont <= 3; cont++){
-      pontos = pontos + matriz_jogo['c'][i];
+      pontos = pontos + matriz_jogo['c'][cont];
     }
     vencedor(pontos);
 
     //Verificador na vertical
-    for(var cont2 = 1; cont <=3; cont2++){
+    for(var cont2 = 1; cont2 <=3; cont2++){
       pontos = 0;
       pontos += matriz_jogo['a'][cont2];
       pontos += matriz_jogo['b'][cont2];
@@ -110,9 +111,27 @@ $(document).ready(function(){
     //Verificador na diagonal
     pontos = 0;
     pontos = matriz_jogo['a'][1] + matriz_jogo['b'][2] + matriz_jogo['c'][3];
+    vencedor(pontos);
 
     pontos = 0;
     pontos = matriz_jogo['a'][3] + matriz_jogo['b'][2] + matriz_jogo['c'][1];
+    vencedor(pontos);
+  }
+
+  function vencedor(pontos) {
+
+    if(pontos == -3) {
+      var primeiroJogador = $("#jogador-um").val();
+      alert (primeiroJogador + 'é o vencedor !');
+      $('.espaco').off();
+    } else if(pontos == 3) {
+        var segundoJogador = $("#jogador-dois").val();
+        alert (segundoJogador + 'é o vencedor !');
+        $('.espaco').off();
+    }
 
   }
+
+
+
 });
